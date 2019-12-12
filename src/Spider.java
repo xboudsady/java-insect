a5ef9c07b8c338b765e8b7f8b6e7723ed01d628d
xboudsady <boudsadyx@gmail.com>
2019-12-11 21:20:57 -0800

    Added Spider class.

---
+++ src/Spider.java
@@ -0,0 +1,14 @@
+public class Spider extends Insect {
+
+  boolean isPoisonous;
+
+  public Spider(int age, boolean isPoisonous) {
+    super(age, 8);
+    this.isPoisonous = isPoisonous;
+  }
+
+  public void says() {
+    System.out.println("HISSSS");
+  }
+
+}
