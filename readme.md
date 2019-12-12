cd4d874a4065e105d250028d62b1f7ed1b9ded86
xboudsady <boudsadyx@gmail.com>
2019-12-11 21:47:50 -0800

    Main class

--- readme.md
+++ readme.md
@@ -3,3 +3,13 @@
 * Inheritence is when one class acquires the methods and fields of another
 
 * The class which inherits the properties of the other is known as a sublass (or derived class, parent class)
+
+* Every object in Java inheris the Object class implicitly
+
+## Implementing Inheritance
+
+* Keyword extends
+
+* Uses of a class extend to inherit the properties of another class
+
+
---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import java.lang.Math;
+import java.util.Random;
+
+public class Main {
+
+  public static void main(String[] args) {
+     Insect insect = new Insect(5, 6);
+     Spider spider = new Spider(13, true);
+     Cricket cricket = new Cricket(2, 1.25);
+
+    insect.crawl();
+    insect.says();
+
+    spider.crawl();
+    spider.says();
+
+    cricket.crawl();
+    cricket.says();
+    cricket.jump();
+
+  }
+
+}
